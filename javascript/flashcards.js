010d2e10189e6ffaf87cbfdaf5bedc0596f04870
nwvbug <nwvbug@gmail.com>
2023-08-02 15:04:17 -0700

    Multiple choice adheres to lang 5 design, also has timer

---
+++ javascript/flashcards.js
@@ -0,0 +1,14 @@
+function doFlashcards(){
+    console.log("Flashcarding")
+    document.getElementById("listview").style.display = "none"
+    showElement(document.getElementById("flashcardBox"))
+
+}
+
+
+
+
+
+function flipCard(){
+
+}
